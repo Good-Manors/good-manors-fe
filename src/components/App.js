613a4e2b7d0786186a41aefb33443e1c603c0153
Luke Donahue <luke9389@gmail.com>
2019-12-05 15:13:49 -0800

    first stab at auth zero

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import NavBar from './NavBar';
+import { useAuth0 } from '../react-auth0-spa';
+import { Router, Route, Switch } from 'react-router-dom';
+import Profile from './Profile';
+import history from '../utils/history';
+import PrivateRoute from './PrivateRoute';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+function App() {
+  const { loading } = useAuth0();
+
+  if(loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path='/' exact />
+          <PrivateRoute path='/profile' component={Profile} />
+        </Switch>
+      </Router>
+   
+    </div>
+    
+  );
 }
-  
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import 'regenerator-runtime/runtime';
+import { Auth0Provider } from './react-auth0-spa';
+import config from './auth_config.json';
+import history from './utils/history';
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 render(
-  <App />,
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
+
