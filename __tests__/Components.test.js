308df598d44fa0a05d7b706c4b454e5edfc86ae2
JOHN NELSON <JohnNelson4850@gmail.com>
2019-12-11 14:20:06 -0800

    Still need Elements, Drawers, Cards  tested

---
+++ __tests__/Components.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Search from '../src/components/Search';
+import Menu from '../src/components/Menu';
+import AboutUs from '../src/components/AboutUs';
+import UserForm from '../src/components/user/UserForm';
+import Dashboard from '../src/components/user/Dashboard';
+
+describe('Tests small components', () => {
+  
+  it('Search.js', () => {
+    const wrapper = shallow(<Search />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Menu.js', () => {
+    jest.mock('react-redux', () => ({
+      useDispatch: ()=> { },
+      useSelector: () => ({})
+    }));
+    const getWrapper = (mockStore = createStore()) => mount(
+      <Provider store={mockStore}>
+        <Menu />
+      </Provider>
+    );
+    expect(getWrapper).toMatchSnapshot();
+  });
+
+  it('AboutUs.js', () => {
+    const wrapper = shallow(<AboutUs />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
+
+describe('User folder', () => {
+  it('UserForm.js', () => {
+    jest.mock('react-redux', () => ({
+      useDispatch: ()=> { },
+      useSelector: () => ({})
+    }));
+
+    const getWrapper = (mockStore = createStore()) => mount(
+      <Provider store={mockStore}>
+        <UserForm buttonText={'test'} handleSubmit={()=>{}} />
+      </Provider>
+    );
+    expect(getWrapper).toMatchSnapshot();
+  });
+ 
+  it('Dashboard.js', () => {
+    const wrapper = shallow(<Dashboard />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+});
