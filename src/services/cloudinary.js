76a920d0a43db0b9ee63e5f9ecf54fa61478a938
JOHN NELSON <JohnNelson4850@gmail.com>
2019-12-10 17:52:54 -0800

    Cloudinary uploads, need to get URL back

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,5 +1,3 @@
-// require('dotenv').load();
-const cloudinary = require('cloudinary-core').v2;
 
 function uploadImageToCloudinary(image) {
   const URL = ' https://api.cloudinary.com/v1_1/good-manors/image/upload';
