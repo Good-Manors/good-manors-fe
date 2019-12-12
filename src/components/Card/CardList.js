f281a7a74b58b3dd4e9462bb2d1fb9d8586e7859
Luke Donahue <luke9389@gmail.com>
2019-12-11 16:05:05 -0800

    took out console.log

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -25,8 +25,6 @@ const CardList = ({ drawer }) => {
   const cards = useSelector(state => getCardsByDrawer(state, drawer));
 
   const mappedCards = cards.map((card, i) => {
-    console.log(card);
-    
     return <Card key={i} {...card} edit={false} />;
   });
 
