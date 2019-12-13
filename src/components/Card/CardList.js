d62dafeeae4654387ab0f95cdd958f276d151fa1
allisonbusse <allisonbusse@gmail.com>
2019-12-13 09:21:00 -0800

    css and base url

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import useModal from '../../hooks/useModal';
 import Card from './Card';
 import styles from './CardList.css';
 import PropTypes from 'prop-types';
 import { getCardsByDrawer } from '../../selectors/homeSelectors';
-import { deleteDrawer } from '../../services/homes';
 import NewCardModal from './NewCardModal';
-import { setHome } from '../../actions/homeActions';
 
 
 const CardList = ({ drawer }) => {
 
   const { isShowing, toggle } = useModal();
-  const dispatch = useDispatch();
 
   const cards = useSelector(state => getCardsByDrawer(state, drawer));
 
-  const handleDeleteDrawer = (drawer) => {
-    deleteDrawer(drawer)
-      .then(home => {
-        dispatch(setHome(home));
-      });
-  };
 
   const mappedCards = cards.map((card, i) => {
     return <Card key={i} {...card} edit={false} />;
@@ -32,7 +23,6 @@ const CardList = ({ drawer }) => {
     <section className={styles.CardList}>
       <h2>{drawer.name}</h2>
       <button className={styles.CardButton} onClick={toggle}>+ New Card</button>
-      <button className={styles.DeleteButton} onClick={handleDeleteDrawer}>Delete Drawer</button>
       <NewCardModal isShowing={isShowing} hide={toggle} drawer={drawer} />
       <section className={styles.List}>
         {mappedCards}
